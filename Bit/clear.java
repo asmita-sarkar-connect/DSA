d3906277cb38df123caa52fa3fd0eef4ee574850
Asmita Sarkar <123262142+asmita-sarkar-connect@users.noreply.github.com>
2025-06-30 16:01:14 +0530

    Add files via upload

---
+++ Bit/clear.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class clear {
+  public static void main(String[] args) {
+    int n= 5;
+    int pos =2;
+    int bitMask = 1<< pos;
+    int notBitMask = ~(bitMask);
+
+    int newNumber = notBitMask & n;
+    System.out.println(newNumber);
+  }
+  
+}
