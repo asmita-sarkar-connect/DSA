20480c71a3f938ee8db72918b1026a8a71ea8bbf
Asmita Sarkar <123262142+asmita-sarkar-connect@users.noreply.github.com>
2026-01-09 14:44:15 +0530

    Add files via upload

---
+++ ArrayList/ArrayLists.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class ArrayLists{
+
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    // ArrayList<String> list2 = new ArrayList<String>();
+    // ArrayList<Boolean> list3 = new ArrayList<>();
+
+    // add elements
+    list.add(0);
+    list.add(2);
+    list.add(3);
+
+    System.out.println(list);
+
+    //get elements
+
+    int element = list.get(0); // we get the elements from the index 
+    System.out.println(element);
+
+    //add element in between
+    list.add(1,1);
+    System.out.println(list);
+
+    //set element 
+    list.set(0,4);
+    System.out.println(list);
+
+    //delete element 
+    list.remove(3);
+    System.out.println(list);
+
+    //size
+    int size = list.size();
+    System.out.println(size);
+
+    //loops
+    for(int i=0; i<list.size(); i++){
+      System.out.print(list.get(i));
+    }
+    System.out.println();
+
+    //sorting
+    Collections.sort(list);
+    System.out.println(list);
+    
+
+  }
+
+}
